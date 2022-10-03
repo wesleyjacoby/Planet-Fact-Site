8280cd281338a30be32c6998007217dacaf323ba
Wesley <wesleyjacoby@gmail.com>
2022-10-03 13:52:16 +0200

    Initial setup

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "League Spartan", sans-serif;
+  font-weight: 400;
+  font-size: 16px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 </style>
